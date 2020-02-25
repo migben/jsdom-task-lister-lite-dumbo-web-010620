f3e4595dc12eb029efd8780f4271d3b935884ab7
migben <miguelangelben1990@gmail.com>
2020-02-25 13:09:58 -0500

    finished base requirements for the lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // select your DOM nodes / el
+  const newTaskForm = document.querySelector("#create-task-form");
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const newTaskPriority = document.querySelector("#new-task-priority");
+
+  // ul the new tasks
+  const newTaskUl = document.querySelector("#tasks");
+
+  newTaskForm.addEventListener("submit", event => {
+    event.preventDefault();
+    // stop the form from trying to submit
+    newTask = document.createElement("li")
+    newTask.innerText = newTaskDescription.value;
+
+    newTaskUl.appendChild(newTask);
+    event.target.reset();
+  })
 });
